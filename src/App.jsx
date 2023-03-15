e5c990096b8b08b614e6610c19f5bfc988fe645b
Ravi Wicaksono <raview69@raview69.local>
2023-03-15 20:32:48 +0700

    Add: Pricing on components

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,26 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
+import React from "react";
+import Hero from "./components/hero/Hero";
+import Header from "./components/header/Header";
+import Pricing from "./components/pricing/Pricing";
+import Background from "./assets/background.svg";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div
+      style={{
+        backgroundImage: `url(${Background})`,
+        backgroundPosition: "left",
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+      }}
+      className="h-screen w-screen"
+    >
+      <Header />
+      <Routes>
+        <Route path="/" element={<Hero />} />
+        <Route path="/pricing" element={<Pricing />} />
+      </Routes>
     </div>
   );
 }
