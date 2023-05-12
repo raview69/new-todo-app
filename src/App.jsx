eab9e01daa3379bb9b1feb1ad36946603b048cb0
Ravi <raview69@coding.local>
2023-05-12 13:29:31 +0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,16 @@
 import React from "react";
-import Hero from "./components/hero/Hero";
-import Header from "./components/header/Header";
-import Pricing from "./components/pricing/Pricing";
-import Auth from "./components/auth/Auth";
-import About from "./components/about/About";
-import Contact from "./components/contact/Contact";
-import Background from "./assets/background.svg";
 import { Routes, Route } from "react-router-dom";
+import Header from "./components/header/Header";
+import Dashboard from "./components/dashboard/Dashboard";
 
 function App() {
   return (
-    <div
-      style={{
-        backgroundImage: `url(${Background})`,
-        backgroundPosition: "left",
-        backgroundSize: "cover",
-        backgroundRepeat: "no-repeat",
-      }}
-      className="h-screen w-screen"
-    >
+    <>
       <Header />
       <Routes>
-        <Route path="/" element={<Hero />} />
-        <Route path="/pricing" element={<Pricing />} />
-        <Route path="/auth" element={<Auth />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/" element={<Dashboard />} />
       </Routes>
-    </div>
+    </>
   );
 }
 
--- src/components/about/About.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const About = () => {
-  return (
-    <div className="flex items-center justify-center w-full">THIS IS ABOUT</div>
-  );
-};
-
-export default About;
